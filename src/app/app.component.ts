86e985c308a5404ec62570589e8ca18f52b78fcb
8-Lambda-8 <j.wasle111@gmail.com>
2021-05-19 17:26:06 +0200

    save Tab to cookies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CookieService } from "ngx-cookie-service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'HomeWI';
+  selectedTab: number = 0;
+
+  constructor(private cookieService: CookieService) { }
+
+  ngOnInit(): void {
+    this.selectedTab = +this.cookieService.get("selectedTab") || 0;
+  }
+
+  setTabState(tabId: number): void {
+    this.selectedTab = tabId;
+    this.cookieService.set("selectedTab", "" + this.selectedTab);
+  }
+
+
+
 }
